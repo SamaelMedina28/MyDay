969426a507d16742bc122ad6feeff7b1a47ab410
Samael Medina <SAMAEL@Dominio.TJ>
2022-06-09 12:37:05 -0700

    Agregado de logo version beta

--- js/notas.js
+++ js/notas.js
@@ -2,6 +2,8 @@ const nota = document.getElementById("nota")
 const fondo = document.getElementById("fondo")
 const letra = document.getElementById("letra")
 const button =  document.getElementById("agregar")
+const borrar =  document.getElementById("borrar")
+
 window.addEventListener("load",()=>{
     console.log(letra)
     letra.value = "#ffffff"
@@ -63,22 +65,22 @@ button.addEventListener("click",()=>{
     const objectStore=transaction.objectStore(`ListaNotas`)
     const request=objectStore.add(notas)
 })
-// borrar.addEventListener("click",()=>{
-//     let confirmacion = confirm("Se borraran todas tu notas ¿Deseas continuar?")
-//     if (confirmacion == true) {
-//     const transaction = db.transaction([`ListaNotas`],`readwrite`)
-//     const objectStore = transaction.objectStore(`ListaNotas`)
-//     const request = objectStore.openCursor()
-//     request.onsuccess = (e) =>{
-//         const cursor = e.target.result
-//         location.reload()
-//         if(cursor){
-//             objectStore.clear()
-//             cursor.continue()
-//         }
-//     }
-//     }
-// })
+borrar.addEventListener("click",()=>{
+    let confirmacion = confirm("Se borraran todas tu notas ¿Deseas continuar?")
+    if (confirmacion == true) {
+    const transaction = db.transaction([`ListaNotas`],`readwrite`)
+    const objectStore = transaction.objectStore(`ListaNotas`)
+    const request = objectStore.openCursor()
+    request.onsuccess = (e) =>{
+        const cursor = e.target.result
+        location.reload()
+        if(cursor){
+            objectStore.clear()
+            cursor.continue()
+        }
+    }
+    }
+})
 
 peticion.onerror=()=>{
     console.log(`Error`,error)}
